c79da6f8a831f9cc36b72d6abb853dc329783760
vj-vikmil <viktors.mileika.cc@gmail.com>
2025-11-26 15:10:49 +0000

    Fix base path to /ascii-detect/ in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ascii-cyber-detection-node/',
+  base: '/ascii-detect/',
   plugins: [react()],
   server: {
     host: true
